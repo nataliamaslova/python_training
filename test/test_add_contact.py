7ea230ffe527e8c81fd30cf886de6ce691c26d84
maslova <maslova.nd@gmail.com>
2017-11-21 22:14:36 +0200

    Classes for group and contact control are extracted

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -4,7 +4,6 @@ import pytest
 from fixture.application import Application
 from model.contact import Contact
 
-
 @pytest.fixture
 def app(request):
     fixture = Application()
@@ -13,6 +12,6 @@ def app(request):
 
 def test_add_contact(app):
     app.session.login(username="admin", password="secret")
-    app.create_contact(Contact(firstname="Peter", lastname="Pyatochkin", company="System Group", address="Ukraine, Kiev, Vatslava Gavela blvd., 4",
-                        mobile="+38 050 777 88 99", email="p.pyatochkin@gmail.com", year="1980"))
+    app.contact.create(Contact(firstname="Peter", lastname="Pyatochkin", company="System Group", address="Ukraine, Kiev, Vatslava Gavela blvd., 4",
+                       mobile="+38 050 777 88 99", email="p.pyatochkin@gmail.com", year="1980"))
     app.session.logout()
