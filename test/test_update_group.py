6de2670a174f4409aeb4eee90502b41de96cd662
maslova <maslova.nd@gmail.com>
2017-11-28 11:03:07 +0200

    input group data optimisation added

--- test/test_update_group.py
+++ test/test_update_group.py
@@ -4,3 +4,13 @@ def test_modify_group(app):
     app.session.login(username="admin", password="secret")
     app.group.update_first_group(Group(name="group 44", header="header 44", footer="footer 44"))
     app.session.logout()
+
+def test_modify_group_name(app):
+    app.session.login(username="admin", password="secret")
+    app.group.update_first_group(Group(name="New group"))
+    app.session.logout()
+
+def test_modify_group_header(app):
+    app.session.login(username="admin", password="secret")
+    app.group.update_first_group(Group(header="New header"))
+    app.session.logout()
