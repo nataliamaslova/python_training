f7c40924a967ed48dd5eb8e8db7b7b27d0410d77
maslova <maslova.nd@gmail.com>
2017-11-28 11:26:14 +0200

    do login and logout only once

--- test/test_update_group.py
+++ test/test_update_group.py
@@ -1,16 +1,10 @@
 from model.group import Group
 
 def test_modify_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.update_first_group(Group(name="group 44", header="header 44", footer="footer 44"))
-    app.session.logout()
 
 def test_modify_group_name(app):
-    app.session.login(username="admin", password="secret")
     app.group.update_first_group(Group(name="New group"))
-    app.session.logout()
 
 def test_modify_group_header(app):
-    app.session.login(username="admin", password="secret")
     app.group.update_first_group(Group(header="New header"))
-    app.session.logout()
