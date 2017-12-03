ea0550cbecb43c6a98f9c0edfaa3ee65895dddb1
maslova <maslova.nd@gmail.com>
2017-12-03 14:46:55 +0200

    group lists sort before comparing

--- test/test_create_group.py
+++ test/test_create_group.py
@@ -3,12 +3,20 @@ from model.group import Group
     
 def test_create_group(app):
     old_groups = app.group.get_group_list()
-    app.group.create(Group(name="group 4", header="header 4", footer="footer 4"))
+    group = Group(name="group 4", header="header 4", footer="footer 4")
+    app.group.create(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
 
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+
 def test_create_empty_group(app):
     old_groups = app.group.get_group_list()
-    app.group.create(Group(name="", header="", footer=""))
+    group = Group(name="", header="", footer="")
+    app.group.create(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
+
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
--- test/test_update_group.py
+++ test/test_update_group.py
@@ -1,25 +1,30 @@
 from model.group import Group
 
-def test_update_group(app):
-    if app.group.count() == 0:
-        app.group.create(Group(name="test"))
-    old_groups = app.group.get_group_list()
-    app.group.update_first_group(Group(name="group 44", header="header 44", footer="footer 44"))
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) == len(new_groups)
+# def test_update_group(app):
+#     if app.group.count() == 0:
+#         app.group.create(Group(name="test"))
+#     old_groups = app.group.get_group_list()
+#     app.group.update_first_group(Group(name="group 44", header="header 44", footer="footer 44"))
+#     new_groups = app.group.get_group_list()
+#     assert len(old_groups) == len(new_groups)
 
 def test_update_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
     old_groups = app.group.get_group_list()
-    app.group.update_first_group(Group(name="New group"))
+    group = Group(name="New group")
+    group.id = old_groups[0].id
+    app.group.update_first_group(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
 
-def test_update_group_header(app):
-    if app.group.count() == 0:
-        app.group.create(Group(name="test"))
-    old_groups = app.group.get_group_list()
-    app.group.update_first_group(Group(header="New header"))
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+
+# def test_update_group_header(app):
+#     if app.group.count() == 0:
+#         app.group.create(Group(name="test"))
+#     old_groups = app.group.get_group_list()
+#     app.group.update_first_group(Group(header="New header"))
+#     new_groups = app.group.get_group_list()
+#     assert len(old_groups) == len(new_groups)
