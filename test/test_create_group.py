4d4bcbf3c676594ee43c3b6327b186f6e358f93e
maslova <maslova.nd@gmail.com>
2017-12-03 21:18:08 +0200

    caching and hashing for groups added

--- test/test_create_group.py
+++ test/test_create_group.py
@@ -5,9 +5,10 @@ def test_create_group(app):
     old_groups = app.group.get_group_list()
     group = Group(name="group 4", header="header 4", footer="footer 4")
     app.group.create(group)
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) + 1 == len(new_groups)
 
+    assert len(old_groups) + 1 == app.group.count() # count used as hash function fro preliminary check
+
+    new_groups = app.group.get_group_list()
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
