fd33823ea116e0ebd0cc0174468d1cce6cdcc14d
maslova <maslova.nd@gmail.com>
2017-12-03 12:36:19 +0200

    Load group data from test app as list

--- test/test_create_group.py
+++ test/test_create_group.py
@@ -2,7 +2,13 @@
 from model.group import Group
     
 def test_create_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="group 4", header="header 4", footer="footer 4"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
 def test_create_empty_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="", header="", footer=""))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
--- test/test_update_group.py
+++ test/test_update_group.py
@@ -3,14 +3,23 @@ from model.group import Group
 def test_update_group(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
+    old_groups = app.group.get_group_list()
     app.group.update_first_group(Group(name="group 44", header="header 44", footer="footer 44"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 def test_update_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
+    old_groups = app.group.get_group_list()
     app.group.update_first_group(Group(name="New group"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 def test_update_group_header(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
+    old_groups = app.group.get_group_list()
     app.group.update_first_group(Group(header="New header"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
