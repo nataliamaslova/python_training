dfcc94ad4ee3947759a3bb41c96ddfd42872246a
maslova <maslova.nd@gmail.com>
2017-12-04 22:21:07 +0200

    Randomness added to groups tests

--- fixture/group.py
+++ fixture/group.py
@@ -35,10 +35,13 @@ class GroupHelper:
             wd.find_element_by_name(field_name).clear()
             wd.find_element_by_name(field_name).send_keys(text)
 
-    def update_first_group(self, new_group_data):
+    def update_first_group(self):
+        self.update_modify_group_by_index(self, 0)
+
+    def update_modify_group_by_index(self, index, new_group_data):
         wd = self.app.wd
         self.open_groups_page()
-        self.select_first_group()
+        self.select_group_by_index(index)
         # select Edit group button
         wd.find_element_by_name('edit').click()
         self.fill_group_form(new_group_data)
@@ -51,10 +54,17 @@ class GroupHelper:
         wd = self.app.wd
         wd.find_element_by_name('selected[]').click()
 
+    def select_group_by_index(self, index):
+        wd = self.app.wd
+        wd.find_elements_by_name('selected[]')[index].click()
+
     def delete_first_group(self):
+        self.delete_group_by_index(self, 0)
+
+    def delete_group_by_index(self, index):
         wd = self.app.wd
         self.open_groups_page()
-        self.select_first_group()
+        self.select_group_by_index(index)
         # submit deletion
         wd.find_element_by_name('delete').click()
         self.open_groups_page()
