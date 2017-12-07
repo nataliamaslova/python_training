5ca443f47b582a0b00cd274ecb6e9218ed85b487
maslova <maslova.nd@gmail.com>
2017-12-07 14:44:21 +0200

    Method renamed

--- fixture/contact.py
+++ fixture/contact.py
@@ -42,14 +42,14 @@ class ContactHelper:
     def delete_first_contact(self):
         self.delete_contact_by_index(0)
 
-    def select_group_by_index(self, index):
+    def select_contact_by_index(self, index):
         wd = self.app.wd
         wd.find_elements_by_name('selected[]')[index].click()
 
     def delete_contact_by_index(self, index):
         wd = self.app.wd
         self.open_contact_page()
-        self.select_group_by_index(index)
+        self.select_contact_by_index(index)
         # click on Delete button
         wd.find_element_by_xpath("//div[@id='content']/form[2]/div[2]/input").click()
         # submit deletion dialog
