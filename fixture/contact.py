a0d09ff33abca21d3f65ac5eec9d3b291e5335c4
maslova <maslova.nd@gmail.com>
2017-12-17 10:36:02 +0200

    verification for all contact fields on main page added

--- fixture/contact.py
+++ fixture/contact.py
@@ -79,19 +79,28 @@ class ContactHelper:
 
     contact_cache = None
 
-    def get_contact_list(self):
+    def get_contact_list(self, index):
         if self.contact_cache is None:
             wd = self.app.wd
             self.open_contact_page()
             self.contact_cache = []
-            for row in wd.find_elements_by_name("entry"):
-                id = row.find_element_by_name("selected[]").get_attribute("value")
-                columns = row.find_elements_by_css_selector("td")
+            rows = wd.find_elements_by_name("entry")
+            for cell in rows[index]:
+                id = cell.find_element_by_name("selected[]").get_attribute("value")
+                columns = cell.find_elements_by_css_selector("td")
                 lastname = columns[1].text
                 firstname = columns[2].text
+                address = columns[3].text
+                emails = columns[4].find_elements_by_css_selector("a")
+                all_emails = ''
+                if (emails.count != 0):
+                    for email in emails:
+                        all_emails = all_emails + email.text + '\n'
                 all_phones = columns[5].text
                 self.contact_cache.append(Contact(firstname = firstname, lastname = lastname, id = id,
-                                                  all_phones_from_home_page = all_phones))
+                                                  address = address,
+                                                  all_phones_from_home_page = all_phones,
+                                                  all_emails_from_home_page = all_emails))
         return list(self.contact_cache)
 
     def open_contact_to_edit_by_index(self, index):
@@ -114,13 +123,19 @@ class ContactHelper:
         firstname = wd.find_element_by_name("firstname").get_attribute("value")
         lastname = wd.find_element_by_name("lastname").get_attribute("value")
         id = wd.find_element_by_name("id").get_attribute("value")
+        address = wd.find_element_by_name("address").get_attribute("value")
         homephone = wd.find_element_by_name("home").get_attribute("value")
         workphone = wd.find_element_by_name("work").get_attribute("value")
         mobilephone = wd.find_element_by_name("mobile").get_attribute("value")
         secondaryphone = wd.find_element_by_name("phone2").get_attribute("value")
+        email = wd.find_element_by_name("email").get_attribute("value")
+        email2 = wd.find_element_by_name("email2").get_attribute("value")
+        email3 = wd.find_element_by_name("email3").get_attribute("value")
         return Contact(firstname=firstname, lastname=lastname, id=id,
+                       address=address,
                        homephone=homephone, workphone=workphone,
-                       mobilephone=mobilephone, secondaryphone=secondaryphone)
+                       mobilephone=mobilephone, secondaryphone=secondaryphone,
+                       email=email, email2=email2, email3=email3)
 
     def get_contact_info_from_view_page(self, index):
         wd = self.app.wd
--- model/contact.py
+++ model/contact.py
@@ -5,7 +5,9 @@ class Contact:
     def __init__(self, firstname = None, lastname = None, company = None, address = None,
                  homephone = None, mobilephone = None, workphone = None, secondaryphone = None,
                  all_phones_from_home_page = None,
-                 email = None, year = None, id = None):
+                 email = None, email2 = None, email3 = None,
+                 all_emails_from_home_page = None,
+                 year = None, id = None):
          self.firstname = firstname
          self.lastname = lastname
          self.company = company
@@ -16,6 +18,9 @@ class Contact:
          self.secondaryphone = secondaryphone
          self.all_phones_from_home_page = all_phones_from_home_page
          self.email = email
+         self.email2 = email2
+         self.email3 = email3
+         self.all_emails_from_home_page = all_emails_from_home_page
          self.year = year
          self.id = id
 
@@ -23,7 +28,8 @@ class Contact:
          return "%s:%s:%s" % (self.id, self.firstname, self.lastname)
 
     def __eq__(self, other):
-         return (self.id is None or other.id is None or self.id == other.id) and self.firstname == other.firstname and self.lastname ==other.lastname
+         return (self.id is None or other.id is None or self.id == other.id) and \
+                self.firstname == other.firstname and self.lastname ==other.lastname
 
     def id_or_max(contact):
         if contact.id:
