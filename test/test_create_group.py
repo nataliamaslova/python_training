2b3383a88379bf385a3f0eacb108d8867c43d877
maslova <maslova.nd@gmail.com>
2017-12-24 09:00:35 +0200

    create group tests parametrized

--- test/test_create_group.py
+++ test/test_create_group.py
@@ -1,22 +1,31 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
-    
-def test_create_group(app):
+import pytest
+import random
+import string
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " " * 10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+testdata = [
+    Group(name="", header="", footer="")] + [
+    Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 20))
+    for i in range(5)
+
+        # alternative: 8 combinations
+        # Group(name=name, header=header, footer=footer)
+        # for name in ["", random_string("name", 10)]
+        # for header in ["", random_string("header", 20)]
+        # for footer in ["", random_string("footer", 20)]
+    ]
+
+@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
+def test_create_group(app, group):
     old_groups = app.group.get_group_list()
-    group = Group(name="group 4", header="header 4", footer="footer 4")
     app.group.create(group)
     assert len(old_groups) + 1 == app.group.count() # count used as a hash function for preliminary check
 
     new_groups = app.group.get_group_list()
-    old_groups.append(group)
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-
-def test_create_empty_group(app):
-    old_groups = app.group.get_group_list()
-    group = Group(name="", header="", footer="")
-    app.group.create(group)
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) + 1 == len(new_groups)
-
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
