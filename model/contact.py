55a95e7fec79b6d1c920caae8de9ec5ef7b67ddc
maslova <maslova.nd@gmail.com>
2017-12-24 10:29:45 +0200

    create contact tests parametrized

--- model/contact.py
+++ model/contact.py
@@ -25,7 +25,8 @@ class Contact:
          self.id = id
 
     def __repr__(self):
-         return "%s:%s:%s" % (self.id, self.firstname, self.lastname)
+         return "%s:%s:%s:%s:%s:%s:%s" % (self.id, self.firstname, self.lastname, self.company, self.address,
+                                    self.mobilephone, self.email)
 
     def __eq__(self, other):
          return (self.id is None or other.id is None or self.id == other.id) and \
--- test/test_create_contact.py
+++ test/test_create_contact.py
@@ -1,10 +1,33 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
+import pytest
+import random
+import string
 
-def test_create_contact(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " " * 10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+def random_phone(maxlen):
+    symbols = string.digits + '()+' + " -" * 3
+    return "".join([random.choice(symbols) for i in range(maxlen)])
+
+def random_email(maxlen):
+    symbols = string.ascii_letters + string.digits + '@._'
+    return "email" + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdata = [
+    Contact(firstname=random_string("firstname", 15), lastname=random_string("lastname", 15),
+            company=random_string("company", 20), address=random_string("address", 40),
+            mobilephone=random_phone(17), email=random_email(30))
+    for i in range(5)
+    ]
+
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_create_contact(app,contact):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="Peter", lastname="Pyatochkin", company="System Group", address="Ukraine, Kiev, Vatslava Gavela blvd., 4",
-                      mobilephone="+38 050 777 88 99", email="p.pyatochkin@gmail.com", year="1980")
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
 
