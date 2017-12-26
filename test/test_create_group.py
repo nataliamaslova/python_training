75af60bd4fb968e371830574073b7215b3f78809
maslova <maslova.nd@gmail.com>
2017-12-26 23:09:38 +0200

    load data from json

--- test/test_create_group.py
+++ test/test_create_group.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
 
-def test_create_group(app, data_groups):
-    group = data_groups
+def test_create_group(app, json_groups):
+    group = json_groups
     old_groups = app.group.get_group_list()
     app.group.create(group)
     assert len(old_groups) + 1 == app.group.count() # count used as a hash function for preliminary check
