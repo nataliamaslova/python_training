b68f3c7399e93df8b228c67df2fea81f60770280
maslova <maslova.nd@gmail.com>
2017-12-28 11:00:18 +0200

    test contact data loading implemented

--- test/test_create_contact.py
+++ test/test_create_contact.py
@@ -1,32 +1,8 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
-import pytest
-import random
-import string
 
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " " * 10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-def random_phone(maxlen):
-    symbols = string.digits + '()+' + " -" * 3
-    return "".join([random.choice(symbols) for i in range(maxlen)])
-
-def random_email(maxlen):
-    symbols = string.ascii_letters + string.digits + '@._'
-    return "email" + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-testdata = [
-    Contact(firstname=random_string("firstname", 15), lastname=random_string("lastname", 15),
-            company=random_string("company", 20), address=random_string("address", 40),
-            mobilephone=random_phone(17), email=random_email(30))
-    for i in range(5)
-    ]
-
-
-@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
-def test_create_contact(app,contact):
+def test_create_contact(app, json_contacts):
+    contact = json_contacts
     old_contacts = app.contact.get_contact_list()
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
