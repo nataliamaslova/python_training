374ae836be7042cb0503bf5338069abad47394fd
maslova <maslova.nd@gmail.com>
2017-12-29 15:43:44 +0200

    test db connect

---
+++ check_db_connection.py
@@ -0,0 +1,13 @@
+# import mysql.connector
+import pymysql.cursors
+
+connection = pymysql.connect(host="localhost", database="addressbook", user="root", password="")
+
+try:
+    pass
+    # cursor = connection.cursor()
+    # cursor.execute("select * from group_list")
+    # for row in cursor.fetchall():
+    #     print(row)
+finally:
+    connection.close()
