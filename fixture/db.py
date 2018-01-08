667aad90aa25d8b19606b6b7d96367397c4aebc5
maslova <maslova.nd@gmail.com>
2018-01-08 12:06:32 +0200

    all contacts on home page are verified by comparing with db data

--- fixture/db.py
+++ fixture/db.py
@@ -33,10 +33,12 @@ class DbFixture():
         list = []
         cursor = self.connection.cursor()
         try:
-            cursor.execute("select id, firstname, lastname from addressbook where deprecated = '0000-00-00 00:00:00'")
+            cursor.execute("select id, firstname, lastname, address, email, email2, email3, home, mobile, work, phone2 from addressbook where deprecated = '0000-00-00 00:00:00'")
             for row in cursor:
-                (id, firstname, lastname) = row
-                list.append(Contact(id=str(id), firstname=firstname, lastname=lastname))
+                (id, firstname, lastname, address, email, email2, email3, home, mobile, work, phone2) = row
+                list.append(Contact(id=str(id), firstname=firstname, lastname=lastname, address=address,
+                                    email=email, email2=email2, email3=email3, homephone=home, mobilephone=mobile, workphone=work, secondaryphone=phone2))
+
         finally:
             cursor.close()
         return list
---
+++ test/test_all_contacts_info.py
@@ -0,0 +1,28 @@
+import re
+from model.contact import Contact
+
+def test_contact_info_on_home_page(app, db):
+    contacts_from_home_page = sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
+    contacts_from_db = sorted(db.get_contact_list(), key=Contact.id_or_max)
+    for i in range(len(contacts_from_home_page)):
+        contact_from_db = contacts_from_db[i]
+        contact_from_home_page = contacts_from_home_page[i]
+        assert contact_from_home_page.firstname == contact_from_db.firstname
+        assert contact_from_home_page.lastname == contact_from_db.lastname
+        assert contact_from_home_page.address == contact_from_db.address
+        assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_db)
+        assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_db)
+
+def clear(s):
+    return re.sub("[() -]", "", s)
+
+def merge_phones_like_on_home_page(contact):
+    return "\n".join(filter(lambda x: x != "",
+                            map(lambda x: clear(x),
+                                filter(lambda x: x is not None,
+                                    [contact.homephone, contact.mobilephone, contact.workphone, contact.secondaryphone]))))
+
+def merge_emails_like_on_home_page(contact):
+    return "\n".join(filter(lambda x: x != "",
+                                filter(lambda x: x is not None,
+                                    [contact.email, contact.email2, contact.email3])))
